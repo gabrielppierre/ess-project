ef58619bd3c8cb32ecada25a9ee13dc7380d3b9d
Gabriel Vasconcelos <ghv@cin.ufpe.br>
2024-06-14 17:46:02 -0300

    fix: add get function

--- backend/src/db/schemas/reservation_schema.py
+++ backend/src/db/schemas/reservation_schema.py
@@ -43,3 +43,9 @@ class ReservationSchema(ModelSchema):
       "description": "The reservation's creation time"
     }
   }
+  def get(self) -> dict:
+    return {
+        "bson_type": self.bson_type,
+        "required": self.required,
+        "properties": self.properties
+    }
