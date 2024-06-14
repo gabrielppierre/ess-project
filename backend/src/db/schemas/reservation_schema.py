74888468ae90894ae01de931092a530ab16281bd
Gabriel Vasconcelos <ghv@cin.ufpe.br>
2024-06-14 13:33:27 -0300

    fix: update reservation schemas

--- backend/src/db/schemas/reservation_schema.py
+++ backend/src/db/schemas/reservation_schema.py
@@ -4,7 +4,7 @@ from src.db.schemas.model_schema import ModelSchema
 
 class ReservationSchema(ModelSchema):
   bson_type: str = "object"
-  required: list = ["id", "room_id", "user_id", "start_date", "end_date"]
+  required: list = ["id", "room_id", "user_id", "start_date", "end_date", "room_name", "activity", "teacher"]
   properties: dict = {
     "id": {
       "bson_type": "string",
@@ -25,6 +25,18 @@ class ReservationSchema(ModelSchema):
     "end_date": {
       "bson_type": "string",
       "description": "The reservation's end date"
+    },
+    "room_name": {
+            "bson_type": "string",
+            "description": "Room's name"
+        },
+    "activity": {
+        "bson_type": "string",
+        "description": "Activity that will be done"
+    },
+    "teacher": {
+        "bson_type": "string",
+        "description": "Associated teacher"
     },
     "created_at": {
       "bson_type": "string",
--- backend/src/schemas/reservation.py
+++ backend/src/schemas/reservation.py
@@ -2,11 +2,21 @@ from typing import Optional
 from datetime import datetime
 from pydantic import BaseModel
 
-class ReservationModel(BaseModel):
+class ReservationCreate(BaseModel):
     room_id: str
     user_id: str
     start_time: datetime
-    end_time: datetime
+    end_time: datetime 
+    room_name: str
+    activity: str
+    teacher: str
+
+class ReservationUpdate(BaseModel):
+    start_time: Optional[datetime]
+    end_time: Optional[datetime] 
+    room_name: Optional[str]
+    activity: Optional[str]
+    teacher: Optional[str]
 
 class ReservationGet(BaseModel):
     id: str
@@ -14,6 +24,9 @@ class ReservationGet(BaseModel):
     user_id: str
     start_time: datetime
     end_time: datetime
+    room_name: str
+    activity: str
+    teacher: str
     created_at: Optional[datetime]
 
 class ReservationList(BaseModel):
