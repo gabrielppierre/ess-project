bc6e87589d50c82e13359e27624fc6b56c6ccd2f
Filipe Eduardo <fefs@cin.ufpe.br>
2024-06-17 01:00:32 -0300

    feat: improve reservation service

--- backend/src/service/impl/reservation_service.py
+++ backend/src/service/impl/reservation_service.py
@@ -24,16 +24,29 @@ class ReservationService(ReservationServiceMeta):
   def create_reservation(reservation: dict) -> HttpResponseModel:
         """Create item method implementation"""
         db.insert_item('reservations', reservation)
+        reservations = db.get_all_items('reservations')
         return HttpResponseModel(
             message=HTTPResponses.RESERVATION_CREATED().message,
             status_code=HTTPResponses.RESERVATION_CREATED().status_code,
+            data=reservations,
     )
   
   @staticmethod
   def update_reservation(reservation_id: str, reservation: dict) -> HttpResponseModel:
         """Update item method implementation"""
+        item = db.get_item('reservations', reservation_id)
+
+        if not item:
+            return HttpResponseModel(
+                message=HTTPResponses.RESERVATION_NOT_FOUND().message,
+                status_code=HTTPResponses.RESERVATION_NOT_FOUND().status_code,
+            )
+
         db.update_item('reservations', reservation_id, reservation)
+
+        reservations = db.get_all_items('reservations')
         return HttpResponseModel(
             message=HTTPResponses.RESERVATION_UPDATED().message,
             status_code=HTTPResponses.RESERVATION_UPDATED().status_code,
+            data=reservations,
     )
